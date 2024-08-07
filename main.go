0125ed9a9e0f648bea936d391390486a718d1e40
Gabe Cook <gabe565@gmail.com>
2024-08-06 19:20:10 -0500

    fix: Fix unformatted log if no args are given

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/charmbracelet/log"
 	"github.com/gabe565/ransom/cmd"
 )
 
 func main() {
+	slog.SetDefault(slog.New(log.New(os.Stderr)))
 	if err := cmd.New().Execute(); err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
