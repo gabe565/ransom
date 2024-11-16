4036ad3084afbf121c8742d87dd458d461615acc
Gabe Cook <gabe565@gmail.com>
2024-11-16 00:15:02 -0600

    chore: Clean up flag error handling with must library

--- internal/config/completions.go
+++ internal/config/completions.go
@@ -1,20 +1,16 @@
 package config
 
 import (
-	"errors"
-
+	"gabe565.com/utils/must"
 	"github.com/spf13/cobra"
 )
 
 func (c *Config) RegisterCompletions(cmd *cobra.Command) {
-	if err := errors.Join(
-		cmd.RegisterFlagCompletionFunc(FlagCompletion, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-			return []string{"bash", "zsh", "fish", "powershell"}, cobra.ShellCompDirectiveNoFileComp
-		}),
-		cmd.RegisterFlagCompletionFunc(FlagPrefix, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-			return []string{"alphabet-white", "alphabet-yellow"}, cobra.ShellCompDirectiveNoFileComp
-		}),
-	); err != nil {
-		panic(err)
-	}
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagCompletion, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+		return []string{"bash", "zsh", "fish", "powershell"}, cobra.ShellCompDirectiveNoFileComp
+	}))
+
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagPrefix, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+		return []string{"alphabet-white", "alphabet-yellow"}, cobra.ShellCompDirectiveNoFileComp
+	}))
 }
